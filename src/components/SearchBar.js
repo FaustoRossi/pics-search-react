5b3f4f25ec62fe5e403729aafc900e9ca818fbe1
Fausto Rossi <fausto.rossi@usal.edu.ar>
2022-07-19 05:49:27 -0300

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,28 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "" };
+	state = { term: "" };
 
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    this.props.onSubmit(this.state.term);
-  };
+	onFormSubmit = (event) => {
+		event.preventDefault();
+		this.props.onSubmit(this.state.term);
+	};
 
-  render() {
-    return (
-      <div className="ui inverted segment">
-        <form onSubmit={this.onFormSubmit} className="ui inverted form">
-          <div className="field">
-            <label>Image Search</label>
-            <input
-              type="text'"
-              value={this.state.value}
-              onChange={(e) => this.setState({ term: e.target.value })}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="ui inverted segment">
+				<form onSubmit={this.onFormSubmit} className="ui inverted form">
+					<div className="field">
+						<label>Image Search</label>
+						<div class="ui inverted divider"></div>
+						<div class="ui inverted left icon input">
+							<input type="text" placeholder="Search..." />
+							<i class="search icon"></i>
+						</div>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 export default SearchBar;
